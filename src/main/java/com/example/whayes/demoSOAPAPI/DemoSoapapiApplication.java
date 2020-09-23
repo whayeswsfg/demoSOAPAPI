880623cb9ff9eae807248b7c2de22b1aaa13623f
Wonbay <54728522+Wonbay@users.noreply.github.com>
2020-09-23 00:51:13 -0400

    Initial commit

---
+++ src/main/java/com/example/whayes/demoSOAPAPI/DemoSoapapiApplication.java
@@ -0,0 +1,13 @@
+package com.example.whayes.demoSOAPAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSoapapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSoapapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/whayes/demoSOAPAPI/DemoSoapapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.whayes.demoSOAPAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSoapapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
